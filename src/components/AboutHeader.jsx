250dccbab6fb6daa2bbe215c9d30da71732b7b3d
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-07-30 15:39:02 +0500

    nukus-avenue-app

--- src/components/AboutHeader.jsx
+++ src/components/AboutHeader.jsx
@@ -4,14 +4,21 @@ const AboutHeader = () => {
   return (
     <div className='AboutHeader'>
         <div className="container">
-            <div className="row">
-                <div className="col-lg-6">
+            <div className="row align-items-center">
+                <div className="col-lg-6 myCol">
                     <div className="cards">
                         <h1>Наш взгляд на жизнь</h1>
                         <p>Строительство — это не просто бизнес! Это то, как мы видим жизнь, наше отношение к жизни. Мы верим в человека и это вдохновляет нас на новые свершения. Архитектурный проект BEST HOUSE — жилой комплекс «Nukus Avenue».</p>
                     </div>
                     <div className="leftImg">
                         <img src="/image/header_b.png" alt="" />
+                    </div>
+                </div>
+
+                <div className="col-lg-6">
+                    <div className="img">
+                        <i><img src="/icon/play.svg" alt="" /></i>
+                        <img className='w-100' src="/image/headerVideo.png" alt="" />
                     </div>
                 </div>
             </div>
--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import AboutHeader from '../components/AboutHeader'
 import AboutUs from '../components/AboutUs'
 import Future from '../components/Future'
 
 const AboutUsPage = () => {
     return (
         <>
+            <AboutHeader />
             <AboutUs />
             <Future />
         </>
