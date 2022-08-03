80cea04b328beb26f151ab1cca562ff9ee4f6294
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-08-03 10:05:39 +0500

    nukus-avenue-app

---
+++ src/components/Plan.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Plan = () => {
+  return (
+    <div className='Plan'>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <h1>План и квадратура квартир</h1>
+                    <div className="img">
+                        <img src="/image/plan.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Plan
--- src/pages/Complexes.jsx
+++ src/pages/Complexes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ComplexesHeader from '../components/ComplexesHeader'
 import Future from '../components/Future'
+import Plan from '../components/Plan'
 
 const Complexes = () => {
     return (
         <>
             <ComplexesHeader />
+            <Plan />
             <Future />
         </>
     )
