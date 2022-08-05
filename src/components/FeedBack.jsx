1a166d98b16d13673e9db3a3d4e67a46412acb54
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-08-05 16:35:52 +0500

    nukus-avenue-app

--- src/components/FeedBack.jsx
+++ src/components/FeedBack.jsx
@@ -4,6 +4,7 @@ import axios from 'axios'
 const FeedBack = ({ modal, setModal }) => {
     const [name, setName] = useState('')
     const [phone_number, setPhoneNumber] = useState('')
+    const [success, setSuccess] = useState(false)
 
     let bot = {
         TOKEN: "5333475266:AAGBTzU1DHiqD080z1Z0un851xYtIxE6ECw",
@@ -16,24 +17,39 @@ const FeedBack = ({ modal, setModal }) => {
 
         axios.get(`https://api.telegram.org/bot${bot.TOKEN}/sendMessage?chat_id=${bot.chatID}&text=${'Имя: ' + name + '%0AТелефон: ' + phone_number}`)
             .then((res) => {
-                console.log(res);
+                setSuccess(true)
+                setModal(false)
+                setTimeout(() => {
+                    setSuccess(false)
+                    setName('')
+                    setPhoneNumber('')
+                }, 2000)
             })
             .catch((err) => {
                 console.log(err);
             })
     }
     return (
-        <div className={`FeedBack ${modal ? 'active' : ''}`}>
-            <div className="cards">
-                <h1>Оставить заявку</h1>
-                <form onSubmit={sendForm}>
-                    <input required autoComplete='off' value={name} onChange={(e) => setName(e.target.value)} className='form-control' type="text" placeholder='Имя' />
-                    <input required autoComplete='off' value={phone_number} onChange={e => setPhoneNumber(e.target.value)} className='form-control' type="number" placeholder='Телефон' />
-                    <button type='submit' className="btn">Отправить</button>
-                </form>
+        <>
+            <div className={`FeedBack ${modal ? 'active' : ''}`}>
+                <div className="cards">
+                    <h1>Оставить заявку</h1>
+                    <form onSubmit={sendForm}>
+                        <input required autoComplete='off' value={name} onChange={(e) => setName(e.target.value)} className='form-control' type="text" placeholder='Имя' />
+                        <input required autoComplete='off' value={phone_number} onChange={e => setPhoneNumber(e.target.value)} className='form-control' type="number" placeholder='Телефон' />
+                        <button type='submit' className="btn">Отправить</button>
+                    </form>
+                </div>
+                <div onClick={() => setModal(false)} className="close"></div>
             </div>
-            <div onClick={() => setModal(false)} className="close"></div>
-        </div>
+
+            <div className={`Check ${success ? 'active' : ''}`}>
+                <div className="cards">
+                    <img src="/image/check.png" alt="" />
+                    <h1>Мы с вами свяжемся</h1>
+                </div>
+            </div>
+        </>
     )
 }
 
